45acf7c5e86297668b3da96e22956300471e68d2
Dario George <edu.dariogeorge21@gmail.com>
2026-05-07 19:56:10 +0530

    Update next.config.js for pdfjs-dist tracing
    Added outputFileTracingIncludes for pdfjs-dist worker and fonts.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@napi-rs/canvas", "pdfjs-dist"],
+    // Force Next.js / Vercel to trace and include the pdfjs-dist worker and fonts
+    outputFileTracingIncludes: {
+      "/api/**/*": ["./node_modules/pdfjs-dist/**/*"],
+    },
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
